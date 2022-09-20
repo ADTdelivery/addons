25b14056423c0b5672848f87503b1d5665acf21e
ADTdelivery <adtdeliverydeveloper@gmail.com>
2022-09-20 08:08:25 -0700

    Add list_price to firm

---
+++ fleet_addons/models/fleetCustom.py
@@ -0,0 +1,16 @@
+import logging
+
+from odoo import models, fields, api
+from odoo.http import request
+
+
+class FleetCustomReport(models.Model):
+    _inherit = 'fleet.vehicle'
+
+    def getLines(self):
+        vehicle_price = request.env['product.template'].search([('product_model_id', '=', self.model_id.id)]).read([
+            'list_price'
+        ])
+        total_vehicle = vehicle_price[0]['list_price'] + 2250.0
+        return total_vehicle
+
