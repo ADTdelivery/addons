95b9fc4946ef2adaf0912e6caf8e16a1e3345e76
ADTdelivery <adtdeliverydeveloper@gmail.com>
2022-11-21 10:35:59 -0500

    Change IP server

--- adt_comercial/models/comercial_cuotas.py
+++ adt_comercial/models/comercial_cuotas.py
@@ -223,7 +223,7 @@ class ADTComercialRegisterPayment(models.TransientModel):
             logging.info(" data 1 " + str(self._name))
 
             # Database connection
-            url = 'http://190.232.26.249:8070'
+            url = 'http://190.238.200.63:8070'
             db = 'odoo'
             username = 'rapitash@gmail.com'
             password = 'Krishnna17'
