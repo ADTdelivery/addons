038eb6e61596d8008f86022150bf0117a274be47
ADTdelivery <adtdeliverydeveloper@gmail.com>
2023-01-31 14:59:27 -0500

    Fix price vehicle

--- fleet_addons/models/fleetCustom.py
+++ fleet_addons/models/fleetCustom.py
@@ -11,6 +11,10 @@ class FleetCustomReport(models.Model):
         vehicle_price = request.env['product.template'].search([('product_model_id', '=', self.model_id.id)]).read([
             'list_price'
         ])
-        total_vehicle = vehicle_price[0]['list_price'] + 2250.0
+        if len(vehicle_price) > 0 :
+            total_vehicle = vehicle_price[0]['list_price'] + 2250.0
+        else :
+            total_vehicle = 0
+
         return total_vehicle
 
